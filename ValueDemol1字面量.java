c4a05ae1312107fcf7db45e6cec66672d194bf3d
ENELipenXka <105711151+ENELipenXka@users.noreply.github.com>
2022-08-31 11:32:59 +0800

    Add files via upload

---
+++ ValueDemol1字面量.java
@@ -0,0 +1,29 @@
+public class ValueDemol1{
+	public static void main(String[]args){
+		//掌握常见的数据在代码中的书写
+		
+		//整数
+		System.out.println(666);
+		System.out.println(-777);
+		
+		//小数
+		System.out.println(1.39);
+		System.out.println(-5.23);
+		
+		//字符串
+		System.out.println("尼古拉斯凯奇");
+		System.out.println("程序员");
+		
+		//字符
+		System.out.println('男');
+		System.out.println('女');
+
+		//布尔
+		System.out.println(true);
+		System.out.println(false);
+
+		//空
+		//null不能直接打印，只能以字符串的形式打印
+		System.out.println("null");
+	}
+}
